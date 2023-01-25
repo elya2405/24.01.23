003fe1193b341f84b589da37af077b51eef1fe3c
kanybekova eleonora <eelya250@gmail.com>
2023-01-25 22:52:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,102 @@
+let myName = "Elya";
+let myMom = "Asel";
+let myFavoriteColor = "black";
+let myFavoriteDay = "Friday";
+let myInterest = "money";
+let myNationality = "kyrgyz";
+let myFriend = "Ajar";
+let myTeacher = "Eleman";
+let myPhone = "redmi";
+let myLaptop = "acer";
+
+let myHeight = 165;
+let myWeight = 50;
+let myFavoriteNumber = 1;
+let myFootSize = 37;
+let myMorningStart = 11;
+
+let laptopIsWorking = true;
+
+let plus = 10 + 5;
+let minus = 10 - 5;
+let multiplication = 10 * 5;
+let division = 10 / 5;
+let degree = 10 ** 5;
+let remains = 10 % 5;
+let example1 = 25 ** (1 / 2);
+let example2 = 8 ** (1 / 3);
+
+let example3 = 4 > 8;
+let example4 = 4 < 8;
+let example5 = 4 >= 8;
+let example6 = 4 <= 8;
+let example7 = 4 === 8;
+let example8 = 4 !== 8;
+
+let a1 = 5,
+  b1 = 5,
+  c1 = 5; // можно так писать
+
+let number = 0.3;
+if (number > 0) {
+  console.log("Число " + number + " положительное");
+} 
+else if (number < 0) {
+  console.log("Число " + number + " отрицательное");
+}
+else if (number === 0) {
+  console.log("Число ноль не отрицательное и не положительное")
+}
+else {
+  console.log(number + " не является числом");
+}
+
+// можно писать " ", ' ', и кривые ковычки (вместо + пишем ${ })
+
+// Является ли число 5 четным или нечетным числом.
+let a = "bb";
+if (a % 2 === 0) {
+  console.log("Число " + a + " четное");
+}
+else if (a % 2 === 1) {
+  console.log("Число " + a + " нечетное");
+}
+else {
+  console.log(a + " не число");
+}
+
+// У Васи 212 сомов,(allMoney) хватит ли ему денег на мороденное которая стоит 40 сом (money).
+let allMoney = 212;
+let money = 40;
+if (allMoney >= money) {
+  console.log("хватит")
+}
+else {
+  console.log ("не хватит")
+}
+
+// Вывести каким сезоном является "январь"(month)
+let month = "66";
+if (month === "декабрь" || month === "январь" || month === "февраль") {
+  console.log("Это зима");
+}
+else if (month === "март" || month === "апрель" || month === "май") {
+  console.log("Это весна");
+}
+else if (month === "июнь" || month === "июль" || month === "август") {
+  console.log("Это лето");
+}
+else if (month === "сентябрь" || month === "октябрь" || month === "ноябрь") {
+  console.log("Это осень");
+}
+else {
+  console.log(month + "это не месяц")
+}
+
+// 
+
+
+
+
+
+
